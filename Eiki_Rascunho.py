3e53f800582d933b1e7ece7bd55dae3cbe00d140
Eiki Yamashiro Santos <49211526+EikiSantos@users.noreply.github.com>
2020-03-09 10:11:11 -0300

    Print de duas retas

--- Eiki_Rascunho.py
+++ Eiki_Rascunho.py
@@ -10,7 +10,14 @@ x_1 = 0
 x_2 = 0
 y_1 = 0
 y_2 = 0
-
+x_r1 = 0
+x_r2 = 0
+y_r1 = 0
+y_r2 = 0
+x_l1 = 0
+x_l2 = 0
+y_l1 = 0
+y_l2 = 0
 
 while(True):
 
@@ -22,14 +29,17 @@ while(True):
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    white = "#ffffff"
+    white = "#eb4034"
     white_1, white_2 = aux.ranges(white)
-    mask_white = cv2.inRange(hsv, white_1, white_2)
+    branco_1 = np.array([0, 0, 210], dtype=np.uint8)
+    branco_2 = np.array([255, 40, 255], dtype=np.uint8)
+    mask_white = cv2.inRange(hsv, branco_1, branco_2)
     edges = cv2.Canny(gray,50,150,apertureSize = 3) 
-    lines = cv2.HoughLines(edges,1,np.pi/180, 200) 
+    lines = cv2.HoughLines(mask_white,1,np.pi/180, 200) 
     
-    for r,theta in lines[0]: 
-
+    for i in range(0, len(lines)): 
+        r = lines[i][0][0]
+        theta = lines[i][0][1]
         a = np.cos(theta) 
 
         b = np.sin(theta) 
@@ -48,24 +58,33 @@ while(True):
         
         delta_x = x1-x2
         delta_y = y1-y2
-        print("DELTA X: ", delta_x)
-        print("DELTA Y: ", delta_y)
         coef_angular = delta_y/delta_x
         modulo_coef = ((coef_angular - coef_angular_anterior)**2)**0.5
         if modulo_coef > 0.3:
             coef_angular_anterior = coef_angular
-            print("TESTEEEEE")
+        coef_angular = float(coef_angular)
+        print(' ')
+        print(' ')
         print("COEF ANGULAR: ", coef_angular)
-        print("MODULO COEF: ", modulo_coef)
-        if modulo_coef > 0.5:
-            x_1 = x1
-            x_2 = x2
-            y_1 = y1
-            y_2 = y2
+        print(' ')
+        if coef_angular > 0.9 and coef_angular < 1.61:
+            x_r1 = x1
+            x_r2 = x2
+            y_r1 = y1
+            y_r2 = y2
+            
+            print("------------------------RIGHT----------------------")
+        elif coef_angular > -2.2 and coef_angular < -0.1:
+            x_l1 = x1
+            x_l2 = x2
+            y_l1 = y1
+            y_l2 = y2  
+            print("------------------------LEFT----------------------")
+            
+    cv2.line(mask_white,(x_r1,y_r1), (x_r2,y_r2), (50,0,255),2)
+    cv2.line(mask_white,(x_l1, y_l1), (x_l2,y_l2), (50,0,255),2) 
 
-        cv2.line(gray,(x1,y1), (x2,y2), (0,0,255),2) 
-
-    cv2.imshow('hsv', gray)
+    cv2.imshow('mask_white', mask_white)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
